8156ea173e7ab4c254e304c2e8dcd7bbc6fc9c0d
Mark Wilkinson <markw@illuminae.com>
2025-09-15 13:49:22 +0200

    date picker works.   Categories requires a new kind of widget...

--- app/controllers/helpers.rb
+++ app/controllers/helpers.rb
@@ -1,17 +1,24 @@
-
 module MyHelpers
-    def logged_in?
-      session[:username]
-    end
+  def logged_in?
+    session[:username]
+  end
 
-    def current_user
-      session[:username]
-    end
+  def current_user
+    session[:username]
+  end
 
-    def scss(template, options = {})
-      # Render SCSS using Compass
-      Compass.sass_engine_options[:load_paths] ||= []
-      Compass.sass_engine_options[:load_paths] << File.join(settings.root, 'stylesheets')
-      render :scss, template, options
+  def scss(template, options = {})
+    # Render SCSS using Compass
+    Compass.sass_engine_options[:load_paths] ||= []
+    Compass.sass_engine_options[:load_paths] << File.join(settings.root, 'stylesheets')
+    render :scss, template, options
+  end
+
+  def extract_date(value)
+    if value.respond_to?(:to_s)
+      value.to_s # Handle string or other objects with to_s
+    else
+      '' # Default to empty if no valid date
     end
+  end
 end
